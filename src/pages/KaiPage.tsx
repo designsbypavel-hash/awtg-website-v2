842b0c8756ee1c8ace90283cc6ca5d41bb60007b
Pavel Mondal <pavel.mondal@awtg.co.uk>
2026-06-05 10:14:48 +0100

    Make Cumberland logo visible in Kai strip

--- src/pages/KaiPage.tsx
+++ src/pages/KaiPage.tsx
@@ -1886,7 +1886,7 @@ export default function KaiPage() {
         const items = [
           { src: '/logos/clients/kai-kp.svg',              alt: 'Kaiser Permanente' },
           { src: '/logos/clients/kai-cambridge.svg',       alt: 'Cambridgeshire County Council' },
-          { src: '/logos/clients/kai-cumberland.svg',      alt: 'Cumberland Council' },
+          { src: '/logos/clients/kai-cumberland.svg',      alt: 'Cumberland Council', height: 56 },
           { src: '/logos/clients/kai-british-council.svg', alt: 'British Council' },
           { src: '/logos/clients/kai-borderlands.svg',     alt: 'Borderlands 5G Innovation Region' },
           { src: '/logos/clients/kai-frame1.svg',          alt: 'West Berkshire Council' },
@@ -1910,8 +1910,8 @@ export default function KaiPage() {
                     <img
                       src={item.src}
                       alt={item.alt}
-                      className="h-9 w-auto object-contain opacity-90 hover:opacity-100 transition-opacity duration-300"
-                      style={{ maxWidth: '200px' }}
+                      className="w-auto object-contain opacity-90 hover:opacity-100 transition-opacity duration-300"
+                      style={{ height: item.height ?? 36, maxWidth: '200px' }}
                     />
                   </div>
                 ))}
